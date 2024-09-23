e899d6d771db238f052828e78969e138d13d2cd4
Niraaj10 <nitcode10@gmail.com>
2024-09-23 17:59:08 +0530

    Project section mod

--- Frontend/src/component/Projects.jsx
+++ Frontend/src/component/Projects.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import tailwind from '../assets/Tailwind.png'
+import react from '../assets/ReactJs.png'
+import express from '../assets/express.png'
+import node from '../assets/node.png'
+import mongoDB from '../assets/mongoDB.png'
+import webSocketDark from '../assets/webDark.svg'
+import webSocketLight from '../assets/webLight.svg'
+import AiDark from '../assets/aiDark.svg'
+import AiLight from '../assets/aiLight.svg'
 
 const Projects = () => {
     return (
@@ -35,6 +44,30 @@ const Projects = () => {
                                         and making it a robust tool for stock market analysis.</li>
                                     <li>Technologies Used: React, Node.js, Express, MongoDB, WebSocket.io , JWT Authentication, ReactApexChart, Finnhub API, Tailwind CSS</li>
                                 </div>
+
+                                <div className='mt-5'>
+                                    <div className='p-3 px-7 rounded-xl backdrop-blur-3xl btnBg text-[#C5C5C5] dark:text-[#808080] font-bold shadowLightBG w-fit'> <a href="https://viewchart1.netlify.app/">Visit website</a></div>
+                                    
+                                    <div className='flex gap-3 mt-3 items-center'>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={react} alt="" className='w-7' />
+                                        </div>
+
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                        <img src={webSocketDark} alt="" className='w-7 dark:hidden' />
+                                        <img src={webSocketLight} alt="" className='w-7 hidden dark:block' />                                        
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={express} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={mongoDB} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={tailwind} alt="" className='w-7' />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                             <div className='basis-[50%]'>
@@ -56,7 +89,30 @@ const Projects = () => {
                                     <li>ChicKickzz is a full-stack e-commerce platform designed for sneaker enthusiasts, leveraging the MERN (MongoDB, Express.js, React.js, Node.js) stack.</li>
                                     <li>The platform provides a seamless and intuitive shopping experience, enabling users to browse, select, and purchase sneakers from a curated catalog.</li>
                                     <li>TKey features include a responsive user interface, secure user authentication, dynamic product management,and an integrated admin panel for efficient order and inventory management.</li>
-                                    <li>• The project showcases expertise in full-stack development, RESTful API design, and state management in React.</li>
+                                    <li>The project showcases expertise in full-stack development, RESTful API design, and state management in React.</li>
+                                </div>
+
+                                <div className='mt-5'>
+                                    <div className='p-3 px-7 rounded-xl backdrop-blur-3xl btnBg text-[#C5C5C5] dark:text-[#808080] font-bold shadowLightBG w-fit'> <a href="https://viewchart1.netlify.app/">Visit website</a></div>
+                                    
+                                    <div className='flex gap-3 mt-3 items-center'>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={react} alt="" className='w-7' />
+                                        </div>
+
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={node} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={express} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={mongoDB} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={tailwind} alt="" className='w-7' />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
@@ -80,6 +136,30 @@ const Projects = () => {
                                     daily plans with activities, and location maps.
                                     </li>
                                     <li>Technologies Used - React ,Intergeted ChatGPT AI for trip plannig, Tripadvisor API for Places Information,HTML, CSS</li>
+                                </div>
+
+                                <div className='mt-5'>
+                                    <div className='p-3 px-7 rounded-xl backdrop-blur-3xl btnBg text-[#C5C5C5] dark:text-[#808080] font-bold shadowLightBG w-fit'> <a href="https://viewchart1.netlify.app/">Visit website</a></div>
+                                    
+                                    <div className='flex gap-3 mt-3 items-center'>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={react} alt="" className='w-7' />
+                                        </div>
+
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={AiDark} alt="" className='w-7 dark:hidden' />
+                                            <img src={AiLight} alt="" className='w-7 hidden dark:block' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={express} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={mongoDB} alt="" className='w-7' />
+                                        </div>
+                                        <div className='p-3 rounded-xl w-fit h-fit backdrop-blur-xl bg-gray-100/50 dark:bg-gray-900/10 border border-gray-100 dark:border-[#23232355]'>
+                                            <img src={tailwind} alt="" className='w-7' />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
